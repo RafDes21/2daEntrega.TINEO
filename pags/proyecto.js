a085a5a8cad42ffcf79316d23c97fa1faf7f5558
RafDes21 <israeltineo84@gmail.com>
2022-04-01 17:10:48 -0300

    agrego fetch

--- pags/proyecto.js
+++ pags/proyecto.js
@@ -1,32 +1,47 @@
-function nombreCelulares(marca, modelo, serie, cantidad) {
+const celularesGuardados = []
 
+function renderResumen() {
+    const lista = document.getElementById(`lista`)
+    lista.innerHTML = ''
+
+    celularesGuardados.forEach(celular => {
+        const row = document.createElement(`tr`)
+        const colMarca = document.createElement(`td`)
+        const colModelo = document.createElement(`td`)
+        const colSerie = document.createElement(`td`)
+        const colCantidad = document.createElement(`td`)
+
+        colMarca.innerText = celular.marca
+        colModelo.innerText = celular.modelo
+        colSerie.innerText = celular.id
+        colCantidad.innerText = celular.cantidad
+
+        row.appendChild(colMarca)
+        row.appendChild(colModelo)
+        row.appendChild(colSerie)
+        row.appendChild(colCantidad)
+        lista.appendChild(row)
+    })
+}
+
+function nombreCelulares(marca, modelo, id, cantidad) {
     const celular = {
         marca: marca,
         modelo: modelo,
-        serie: serie,
+        id: id,
         cantidad: cantidad
     }
+    const posicion = celularesGuardados.findIndex(celular => celular.id === id);
+    console.log(posicion)
+    if (posicion > -1) {
+        const celularActualizado = celularesGuardados[posicion];
+        celularActualizado.cantidad += 1;
+        celularesGuardados[posicion] = celularActualizado;
+    } else {
+        celularesGuardados.push(celular)
+    }
+    renderResumen()
 
-
-    const lista = document.getElementById(`lista`)
-    const row = document.createElement(`tr`)
-
-    const colMarca = document.createElement(`td`)
-    const colModelo = document.createElement(`td`)
-    const colSerie = document.createElement(`td`)
-    const colCantidad = document.createElement(`td`)
-
-    colMarca.innerText = marca
-    colModelo.innerText = modelo
-    colSerie.innerText = serie
-    colCantidad.innerText = cantidad
-
-    row.appendChild(colMarca)
-    row.appendChild(colModelo)
-    row.appendChild(colSerie)
-    row.appendChild(colCantidad)
-    lista.appendChild(row)
-    celularesGuardados.push(celular)
 
     Toastify({
         text: "se agrego exitosamete",
@@ -34,29 +49,26 @@ function nombreCelulares(marca, modelo, serie, cantidad) {
     }).showToast()
 }
 
-const celularesGuardados = []
-
 const btn1 = document.querySelector(`.btn1`)
 
 btn1.addEventListener(`click`, () => {
-    nombreCelulares("IPHONE", "A32", "09897", 1)
+    nombreCelulares("IPHONE", "A32", "001", 1)
 
 })
 
 const btn2 = document.querySelector(`.btn2`)
 btn2.addEventListener(`click`, () => {
-    nombreCelulares("SAMSUNG", "A32", "00811", 1)
-
+    nombreCelulares("SAMSUNG", "A32", "002", 1)
 })
 
 const btn3 = document.querySelector(`.btn3`)
 btn3.addEventListener(`click`, () => {
-    nombreCelulares("MOTOROLA", "G9 POWER", "00811", 1)
+    nombreCelulares("MOTOROLA", "G9 POWER", "003", 1)
 })
 
 const btn4 = document.querySelector(`.btn4`)
 btn4.addEventListener(`click`, () => {
-    nombreCelulares("LG", "XT-615", "00811", 1)
+    nombreCelulares("LG", "XT-615", "004", 1)
 })
 
 const boton = document.getElementById(`guardar`)
@@ -126,5 +138,92 @@ finalizar.addEventListener(`click`, () => {
         showConfirmButton: false,
         timer: 3000,
     })
+
+})
+
+// clima
+window.addEventListener(`load`, () => {
+
+    if (navigator.geolocation) {
+
+        let lon
+        let lat
+        const temperaturaValor = document.getElementById(`temperatura-valor`)
+        const temperaturaDescripcion = document.getElementById(`temperatura-descripcion`)
+
+        const Ubicacion = document.getElementById(`ubicacion`)
+        const iconoAnimado = document.getElementById(`icono-animado`)
+
+        const vientoVelocidad = document.getElementById(`viento-velocidad`)
+
+
+
+        navigator.geolocation.getCurrentPosition(posicion => {
+            // console.log(posicion.coords.latitude)
+
+            lon = posicion.coords.longitude
+            lat = posicion.coords.latitude
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=es&appid=85d747fb1f5e882b6627d0863a8c4065`
+            console.log(url);
+
+            fetch(url)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data);
+                    const temp = Math.round(data.main.temp)
+                    temperaturaValor.textContent = `${temp}°C`
+
+                    const desc = data.weather[0].description
+                    temperaturaDescripcion.textContent = desc.toUpperCase()
+
+                    const lugar = data.name
+                    Ubicacion.textContent = lugar
+
+                    const velocidadViento = data.wind.speed
+                    vientoVelocidad.textContent = `${velocidadViento} m/s`
+
+                    console.log(data.weather[0].main);
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                            iconoAnimado.src = 'animated/thunder.svg'
+
+                            break;
+                        case 'Drizzle':
+                            iconoAnimado.src = 'animated/rainy-2.svg'
+
+                            break;
+                        case 'Rain':
+                            iconoAnimado.src = 'animated/rainy-7.svg'
+
+                            break;
+                        case 'Snow':
+                            iconoAnimado.src = 'animated/snowy-6.svg'
+
+                            break;
+                        case 'Clear':
+                            iconoAnimado.src = 'animated/day.svg'
+
+                            break;
+                        case 'Atmosphere':
+                            iconoAnimado.src = 'animated/weather.svg'
+
+                            break;
+                        case 'Clouds':
+                            iconoAnimado.src = 'animated/cloudy-day-1.svg'
+
+                            break;
+                        default:
+                            iconoAnimado.src = 'animated/cloudy-day-1.svg'
+
+                    }
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        })
+    }
 
 })
